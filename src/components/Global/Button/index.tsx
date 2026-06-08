77da7f4ef93b72aa43672bcd2266a49296c6c08e
devNroses <jselbygfx1@gmail.com>
2026-06-07 23:26:13 -0700

    cleaning up animation of scale and hover effect

--- src/components/Global/Button/index.tsx
+++ src/components/Global/Button/index.tsx
@@ -5,10 +5,11 @@ export const Button = () => {
     return (
         <motion.button 
             className={styles.button} 
-            whileHover={{ scale: 1.05 }} 
-            whileTap={{ scale: 0.95 }}>
-            <span data-text="Glitch Button" className={styles.glitch}>
-                Glitch Button
+            whileHover={{ scale: 1.04, transition: { duration: 0.45 } }} 
+            whileTap={{ scale: 1, transition: { duration: 0.45 } }} 
+        >
+            <span data-text="Enter" className={styles.glitch}>
+                Enter
             </span>
         </motion.button>
     )
