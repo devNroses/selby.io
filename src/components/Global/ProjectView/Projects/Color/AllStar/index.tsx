3b4ba6b3f54cbea13a10500a1aa25f6da225ea95
devNroses <jselbygfx1@gmail.com>
2026-07-03 11:57:26 -0700

    fixing layout via css for pageView

--- src/components/Global/ProjectView/Projects/Color/AllStar/index.tsx
+++ src/components/Global/ProjectView/Projects/Color/AllStar/index.tsx
@@ -1,12 +1,120 @@
+import { useRef, useLayoutEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { motion } from 'motion/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import styles from './Allstar.module.css'
 
+gsap.registerPlugin(ScrollTrigger)
+
 export const AllStarProject = () => {
+    const { id } = useParams()
+    const allstarContainerRef = useRef<HTMLDivElement>(null)
+    const allstarIntroRef = useRef<HTMLDivElement>(null)
+    const boxFeatureRef = useRef<HTMLDivElement>(null)
+    const boxaRef = useRef<HTMLDivElement>(null)
+    const boxbRef = useRef<HTMLDivElement>(null)
+    const ctx = useRef<gsap.Context | null>(null)
+
+    useLayoutEffect(() => {
+        if(!allstarContainerRef.current || 
+            !
+allstarIntroRef.current ||
+            !boxaRef.current ||
+            !boxbRef.current
+        ) return
+
+        const introElement = allstarIntroRef.current
+        ctx.current = gsap.context(() => {
+            gsap.fromTo(
+                introElement.querySelectorAll('h2'),
+                { opacity: 0, y: 50 },
+                {
+                    opacity: 1, 
+                    y: 0,
+                    duration: 0.85,
+                    delay: 0.5,
+                    stagger: 0.35,
+                    ease: "circ.out",
+                }
+            );
+            gsap.fromTo(boxaRef.current, 
+                { opacity: 0, height: 0, y: 260 },
+            {
+                opacity: 1,
+                height: 400,
+                duration: 6.5,
+                y: 200,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: allstarIntroRef.current,
+                    scroller: window,
+                    start: "top top",
+                    end: "bottom center",
+                    scrub: true,
+                    toggleActions: "restart pause reverse pause",
+                    // markers: true, // shows start/end markers in browser
+                }
+            }
+            ),
+            gsap.fromTo(boxbRef.current,
+                { opacity: 0, height: 0, y: 200 },
+            {
+                opacity: 1,
+                height: 400,
+                duration: 6.5,
+                y: 150,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: boxaRef.current,
+                    scroller: window,
+                    start: "top 345px",
+                    end: "bottom top",
+                    endTrigger: boxbRef.current,
+                    scrub: true,
+                    toggleActions: "restart pause reverse pause",
+                    // markers: true, // shows start/end markers in browser
+                }
+            }
+            )
+        }, allstarContainerRef.current)
+
+        return () => {
+            ctx.current?.revert() // cleans up on unmount
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill()) // kills all ScrollTriggers
+        }
+    }, [id]) // re-run effect if id changes
+
     return (
-        <motion.div className={styles.allstarContainer}>
-            <motion.div className={styles.allstarIntro}>
-                Project All Star
+        <motion.div 
+            ref={allstarContainerRef}
+            className={styles.allstarContainer}>
+            {/* <motion.div 
+                ref={allstarIntroRef}
+                className={styles.allstarIntro}
+            >
+                <h2>
+                    Project All Star
+                </h2>
+                <div 
+                ref={boxFeatureRef}
+                className={styles.boxa1} />
+                <div 
+                ref={boxaRef}
+                className={styles.boxa} />
+                <div 
+                ref={boxbRef}
+                className={styles.boxb} />
+            </motion.div> */}
+            {/* <motion.div className={styles.section}>
+                Box 1
             </motion.div>
+            <motion.div className={styles.section}>
+                Box 2
+            </motion.div>
+            <motion.div className={styles.section}>
+                Box 3
+            </motion.div> */}
         </motion.div>
     )
 }
--- src/components/Global/ProjectView/index.tsx
+++ src/components/Global/ProjectView/index.tsx
@@ -1,22 +1,19 @@
-import { useParams, useNavigate } from 'react-router-dom';
+// import { useParams, useNavigate } from 'react-router-dom';
 import { motion } from 'motion/react';
 import { AllStarProject } from './Projects/Color/AllStar';
+import styles from './ProjectView.module.css'
 
 export const ProjectView = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
     return (
         <motion.div
-            className="pageSection"
-            initial={{ opacity: 0, scale: 0.98 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.98 }}
+            className={styles.pageSection}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
             transition={{ duration: 0.4, ease: 'easeOut' }}
         >
             {/* <button onClick={() => navigate('/dashboard')}>← Back</button> */}
-            <div>
-                <AllStarProject />
-            </div>
+            <AllStarProject />
         </motion.div>
     )
 }
