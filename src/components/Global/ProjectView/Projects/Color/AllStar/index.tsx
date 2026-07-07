c891a496007cfaab7dd57dfea6e7ec5380262c8e
devNroses <jselbygfx1@gmail.com>
2026-07-06 23:38:43 -0700

    updating archive layout for all star

--- src/components/Global/ProjectView/Projects/Color/AllStar/index.tsx
+++ src/components/Global/ProjectView/Projects/Color/AllStar/index.tsx
@@ -202,14 +202,38 @@ export const AllStarProject = () => {
                 <div ref={horizontalSectionRef} className={styles.horizontalSection}>
                     <div ref={horizontalTrackRef} className={styles.horizontalTrack}>
                         <div className={styles.horizontalPanel}>
-                            <div className={styles.boxlrg}>
-                                 <img src="/portfolioImg/allStar/dBook.jpg" alt="Devin booker in All Star 26 Jacket"/>
+                            <div className={styles.boxlrg} style={{maxWidth: '600px'}}>
+                                 <img src="/portfolioImg/allStar/allstarMoodboard.png" alt="Color moodboard for All Star concept"/>
                             </div>
                             <div className={styles.boxlrg}>
                                  <img 
                                     src="/portfolioImg/allStar/swatches.png" 
                                     alt="Explored swatches for shirt and merch"
                                     style={{ objectPosition: 'center center' }}
+                                />
+                            </div>
+                            <div className={`${styles.boxlrg} ${styles.boxlrgWide}`} style={{maxWidth: '400px'}}>
+                                <video
+                                src={'/portfolioImg/allStar/allstarStripes_26.mov'}
+                                autoPlay
+                                loop
+                                muted
+                                playsInline
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.horizontalPanel}>
+                            <div className={styles.boxlrg}>
+                                 <img src="/portfolioImg/allStar/dBook.jpg" alt="Devin booker in All Star 26 Jacket"/>
+                            </div>
+                            <div className={`${styles.boxlrg}`}>
+                                <video
+                                src={'/portfolioImg/allStar/socialResponse.mp4'}
+                                autoPlay
+                                loop
+                                muted
+                                controls
+                                playsInline
                                 />
                             </div>
                             <div className={styles.boxlrg}>
