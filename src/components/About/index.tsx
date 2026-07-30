fee97aadf9c2cae66824594bdec96de41e231958
devNroses <jselbygfx1@gmail.com>
2026-07-30 01:42:31 -0700

    refactor about to using tab system

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,10 +8,50 @@ export const AboutPage = () => {
   return (
     <div className={styles.aboutWrapper}>
       <div className={styles.contentContainer}>
-        container 1
+        <div className={styles.contentWrapper}>
+          <div className={styles.tabsContainer}>
+            <motion.div
+              style={{ border: '1px solid white'}}
+              initial={{y: '-105%'}}
+              animate={{y: 0}}
+              transition={{ duration: 0.65, ease: 'easeInOut' }}
+            >
+              Creative Navigator
+            </motion.div>
+            <motion.div
+              style={{ 
+                borderTop: '1px solid white',
+                borderBottom: '1px solid white',
+                borderRight: '1px solid white'
+              }}
+              initial={{y: '-105%'}}
+              animate={{y: 0}}
+              transition={{ duration: 0.65, delay: .5, ease: 'easeInOut' }}
+            >
+              Design Engineer
+            </motion.div>
+            <motion.div
+              style={{ 
+                borderTop: '1px solid white',
+                borderBottom: '1px solid white',
+                borderRight: '1px solid white'
+              }}
+              initial={{y: '-105%'}}
+              animate={{y: 0}}
+               transition={{ duration: 0.65, delay: .75, ease: 'easeInOut' }}
+            >
+              Color Designer
+            </motion.div>
+          </div>
+          <div />
+          <div className={styles.tabContent}>
+
+          </div>
+        </div>
+        <img src="/imgs/about/selby_train.jpg" />
       </div>
 
-      <motion.div
+      {/* <motion.div
         className={styles.contentContainer}
         initial={{ opacity: .5, x: 500 }}
         whileInView={{ opacity: 1, x: 0 }}
@@ -29,7 +69,9 @@ export const AboutPage = () => {
             </p>
 
             <div className={styles.aboutSocials}>
-                <motion.a 
+                <motion.a
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.65, delay: 0.85 } }}
                   href="https://www.instagram.com/selby.io/" 
                   target="_blank" 
                   aria-label='selbyio instagram'
@@ -37,13 +79,17 @@ export const AboutPage = () => {
                   <InstagramIcon width={32} height={32} stroke='#E2FFC1'/>
                 </motion.a>
                 <motion.a 
+                  initial={{ opacity: 0, y: 15 }}
+                  animate={{ opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.65, delay: 1 } }}
                   href="https://github.com/devNroses" 
                   target="_blank" 
                   aria-label='selbyio github'
                 >
                   <GithubIcon width={32} height={32} stroke='#E2FFC1' />
                 </motion.a>
-                <motion.a 
+                <motion.a
+                  initial={{ opacity: 0, y: 15 }}
+                  animate={{ opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.65, delay: 1.15 } }}
                   href="https://www.linkedin.com/in/jonathan-selby-developer/" 
                   target="_blank" 
                   aria-label='Selby LinkedIn'
@@ -74,7 +120,55 @@ export const AboutPage = () => {
         <div className={styles.aboutContact}>
             Contact section
         </div>
-      </motion.div>
+      </motion.div> */}
     </div>
   )
 }
+
+
+//  <motion.div>
+//           Show Me
+//         </motion.div>
+//         <motion.div className={styles.aboutInfo}>
+//             <motion.p
+//               initial={{ opacity: 0, y: 10 }}
+//                 animate={{ opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.65, delay: 0.85 } }}
+//             >
+//                 Navigating complexity across digital and tangible experiences. a 
+//                 multidisciplinary creator shaped by an Army upbringing, built 
+//                 to adapt and deliver across mediums.
+//             </motion.p>
+
+//             <div className={styles.aboutSocials}>
+//                 <motion.a
+//                 initial={{ opacity: 0, y: 10 }}
+//                 animate={{ opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.65, delay: 0.85 } }}
+//                   href="https://www.instagram.com/selby.io/" 
+//                   target="_blank" 
+//                   aria-label='selbyio instagram'
+//                 >
+//                   <InstagramIcon width={32} height={32} stroke='#E2FFC1'/>
+//                 </motion.a>
+//                 <motion.a 
+//                   initial={{ opacity: 0, y: 15 }}
+//                   animate={{ opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.65, delay: 1 } }}
+//                   href="https://github.com/devNroses" 
+//                   target="_blank" 
+//                   aria-label='selbyio github'
+//                 >
+//                   <GithubIcon width={32} height={32} stroke='#E2FFC1' />
+//                 </motion.a>
+//                 <motion.a
+//                   initial={{ opacity: 0, y: 15 }}
+//                   animate={{ opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.65, delay: 1.15 } }}
+//                   href="https://www.linkedin.com/in/jonathan-selby-developer/" 
+//                   target="_blank" 
+//                   aria-label='Selby LinkedIn'
+//                 >
+//                   <LinkedInIcon width={32} height={32} stroke='#E2FFC1' />
+//                 </motion.a>
+//             </div>
+//         </motion.div>
+//         <motion.div>
+//           Hello I'm here
+//         </motion.div>
